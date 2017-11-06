1963bd3901f9877ad978cfb3ffce279ac9cd298f
marcinmiszkurka <marcin.miszkurka@interia.pl>
2017-11-06 17:04:39 +0100

    ćwiczenie1

---
+++ cwiczenie1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int a, b;
+int main()
+{
+ 	printf("podaj a \n");
+	scanf("%d", &a);
+	printf("podaj b \n");
+	scanf("%d", &b);
+		if (a>b) {
+		prinf("większe jest a %d" a);}
+		elese if (a<b){
+		printf("większe jest b %d" b);}
+		elese if (a=b){
+		printf("a i b jest równe %d" b);}
+}
+return 0
+}
+
---
+++ notatnik.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int a, b, c, suma, x, y, z;
+
+void main(void)
+{
+printf("podaj a=");
+scanf("%d", &x);
+scanf("%d", &y);
+scanf("%d", &z);
+suma = x + y + z;
+printf("suma = %d\n" , suma);
+}
+
